a80384adf38368de0a776d76216eba4596ca1efa
bookck13 <bookck13@gmail.com>
2023-02-03 12:58:16 -0500

    Initial commit

---
+++ src/test/java/org/example/UserDaoTest.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import com.zaxxer.hikari.HikariConfig;
+import org.example.ConnectionManger;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserDaoTest {
+
+    @BeforeEach
+    void setUp() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("db_schema.sql"));
+        DatabasePopulatorUtils.execute(populator, ConnectionManger.getDataSource());
+
+    }
+
+    @Test
+    void createTest() throws SQLException {
+        UserDao userDao = new UserDao();
+
+        userDao.create(new User("wizard", "password","name","email"));
+
+        User user = userDao.findByUserId("wizard");
+        assertThat(user).isEqualTo(new User("wizard", "password","name","email"));
+
+
+    }
+}
